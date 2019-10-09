d1dc19bb47fe4fd0c03317a2e5ea5f1fc2b47ae0
Breno Freire <breno.loorran@gmail.com>
2019-10-09 19:49:06 -0300

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'index',
     pathMatch: 'full'
   },
   {
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'index', loadChildren: './index/index.module#IndexPageModule' }
 ];
 
 @NgModule({
